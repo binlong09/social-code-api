e7bcb0219dcd0a8ef53e519866e2777c94fe90db
binlong09 <binlong09@gmail.com>
2019-09-24 19:36:15 -0400

    only allow authentication with @gettysburg.edu

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
 class User < ApplicationRecord
   has_secure_password
+
+  EMAIL_REGEX = %r{\A[a-z][a-z0-9_\.]{3,32}@gettysburg.edu\z}
+
   validates :email, presence: true, uniqueness: true
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  # validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :email, format: { with: EMAIL_REGEX }
   validates :password,
             length: { minimum: 6 },
             if: -> { new_record? || !password.nil? }
+
+
 end
