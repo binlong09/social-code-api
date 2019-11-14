d1ecc5332fce9de285be40cae71762282d2ee476
binlong09 <binlong09@gmail.com>
2019-11-13 19:31:07 -0500

    added herokuapp to domain

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV.fetch("SENDGRID_USERNAME"),
   :password => ENV.fetch("SENDGRID_PASSWORD"),
-  :domain => "#{ENV.fetch("HEROKU_APP_NAME")}.com",
+  :domain => "#{ENV.fetch("HEROKU_APP_NAME")}.herokuapp.com",
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
